16ae4dc4ae84c316addf98fa11ea44b7755b01e9
Martxel Aranzadi <maranzadi@innguma.com>
2024-02-22 16:43:35 +0100

    mejora del html y cambio en el sobre-mi

--- scripts.js
+++ scripts.js
@@ -1,11 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("nav ul li a");
-    links.forEach(link => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-            targetElement.scrollIntoView({ behavior: "smooth" });
-        });
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    var nav_values = document.querySelectorAll(".nav_values");
+    if(nav_values){
+        
+        nav_values.forEach(function(nav){
+            nav.addEventListener("click", function(target){
+                if(target.originalTarget.id=="sobre_mi"){
+                    fetch("templates/sobre-mi.html")
+                    .then(response => response.text())
+                    .then(html => {
+                        document.getElementById('section-container').innerHTML = html;
+                    })
+                    .catch(error => console.error('Error al cargar la sección:', error));
+                    return false;
+                }
+                if(target.originalTarget.id=="proyectos"){
+                    fetch("templates/proyectos.html")
+                    .then(response => response.text())
+                    .then(html => {
+                        document.getElementById('section-container').innerHTML = html;
+                    })
+                    .catch(error => console.error('Error al cargar la sección:', error));
+                    return false;
+                }
+                if(target.originalTarget.id=="contacto"){
+                    fetch("templates/contacto.html")
+                    .then(response => response.text())
+                    .then(html => {
+                        document.getElementById('section-container').innerHTML = html;
+                    })
+                    .catch(error => console.error('Error al cargar la sección:', error));
+                    return false;
+                }
+            })
+        })
+    }
 });
